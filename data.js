ee93adeec00c190d879081225b3e0060eddb4e05
Rhys Gregory <88214579+itsrhys754@users.noreply.github.com>
2024-03-19 03:01:03 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+const movies = [
+    {
+        title: 'Spiderman No Way Home',
+        poster: 'img/SpidermanNoWayHome.jpg',
+        genre: 'Action',
+        videoUrl: 'https://vidsrc.xyz/embed/movie/tt10872600'
+    },
+    {
+        title: 'Movie 2',
+        poster: 'https://via.placeholder.com/200x300',
+        genre: 'Comedy',
+        videoUrl: 'https://example.com/movie2.mp4'
+    },
+    {
+        title: 'Movie 3',
+        poster: 'https://via.placeholder.com/200x300',
+        genre: 'Drama',
+        videoUrl: 'https://example.com/movie3.mp4'
+    }
+];
